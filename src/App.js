6ebbf745486e5d35075b9faa2c6067e5a7a3f0b0
vishal gupta <vishalguptadishu@gmail.com>
2023-07-01 19:20:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>benifits of useing react...</h1>
+        <ul>1.Component-based architecture</ul>
+        <ul>2.Virtual DOM for efficient updates</ul>
+        <ul>3.Rich ecosystem and community</ul>
+        <ul>4.Strong community support</ul>
+        <button>Get Started</button>
+      
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
